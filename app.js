50d67d9dbf35298d7081387e776816a5e45b7a8d
srchans1609-stack <srchan1609@gmail.com>
2026-09-16 13:01:44 +0200

    Añadiendo y corrigiendo fallos para el boton

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+
+// 1. Recuperar elementos del DOM
+const boton = document.getElementById('mi-boton');
+const aviso = document.getElementById('aviso');
+
+// 2. Variable contador
+let contador = 0;
+
+// 3. Escuchar clic y actualizar aviso
+boton.addEventListener('click', function() {
+  contador++;
+  aviso.textContent = `Has pulsado el botón ${contador} ${contador === 1 ? 'vez' : 'veces'}.`;
+});
