297e43f7781e4ad594a04fd2bedfa4ef55a19b55
srchans1609-stack <srchan1609@gmail.com>
2026-09-16 13:18:53 +0200

    Añadiendo el modo oscuro

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
 
-// 1. Recuperar elementos del DOM
+
 const boton = document.getElementById('mi-boton');
 const aviso = document.getElementById('aviso');
-
-// 2. Variable contador
 let contador = 0;
+if (boton && aviso) {
+  boton.addEventListener('click', function() {
+    contador++;
+    aviso.textContent = `Has pulsado el botón ${contador} ${contador === 1 ? 'vez' : 'veces'}.`;
+  });
+}
 
-// 3. Escuchar clic y actualizar aviso
-boton.addEventListener('click', function() {
-  contador++;
-  aviso.textContent = `Has pulsado el botón ${contador} ${contador === 1 ? 'vez' : 'veces'}.`;
-});
+const btnModoNoche = document.getElementById('modonoche');
+
+if (btnModoNoche) {
+  btnModoNoche.addEventListener('click', function() {
+    document.body.classList.toggle('tema-claro');
+
+    if (document.body.classList.contains('tema-claro')) {
+      btnModoNoche.textContent = '🌙 Modo oscuro';
+    } else {
+      btnModoNoche.textContent = '☀️ Modo claro';
+    }
+  });
+}
